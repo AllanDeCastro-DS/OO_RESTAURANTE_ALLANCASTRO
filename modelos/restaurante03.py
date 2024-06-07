7a912e21c5ee92e94ed2c1226804a2416d9f7781
allan <allandecastro2007@gmail.com>
2024-06-07 12:26:54 -0300

    Restaurante 03

--- modelos/restaurante03.py
+++ modelos/restaurante03.py
@@ -1,8 +1,27 @@
-restaurante = [];
+
 
 class Restaurantes:
+    restaurantes = [];
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self.nome = nome.title()
+        self.categoria = categoria.upper()
+        self._ativo = False
+        Restaurantes.restaurantes.append(self)
 
+    def __str__(self):
+        return(f'{self.nome} | {self.categoria} | {self.ativo}')
+    @property
+    def ativo(self):
+        return('⊠') if self._ativo == True else '☐'
+
+    @classmethod
+    def listar(cls):
+        print(f'{"Nome do restaurante"} | {"categoria"} | {"status"}')
+        for restauranted in cls.restaurantes:
+            print(f'{restauranted.nome.ljust(20)} | {restauranted.categoria.ljust(20)} | {restauranted.ativo}')
+
+restPraca = Restaurantes('Praça','praça')
+restpizza = Restaurantes('pizza', 'pizza')
+
+
+print(Restaurantes.listar())
